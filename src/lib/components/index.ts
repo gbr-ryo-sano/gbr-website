f33a9fb482e9e38dc69f6e86cea017c47ceea36b
gbr2170 <ry.sano@gbr-net.co.jp>
2025-05-08 10:37:12 +0900

    コンポーネントのエクスポート

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,4 +1,6 @@
 import Header from './Header.svelte';
 import Footer from './Footer.svelte';
+import CompanyHeading from './page/home/CompanyHeading.svelte';
+import BussinessList from './page/home/BussinessList.svelte';
 
-export { Header, Footer };
+export { Header, Footer, CompanyHeading, BussinessList };
