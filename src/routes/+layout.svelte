b06383506d6776534d3c3adaecc7af62dde66d87
gbr2170 <ry.sano@gbr-net.co.jp>
2025-05-08 10:16:21 +0900

    レイアウトファイルの作成

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { Header, Footer } from "$lib/components";
+    let { children } = $props()
+</script>
+
+<div class="layout">
+    <Header />
+    <main>{@render children()}</main>
+    <Footer />
+</div>
